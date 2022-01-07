6bb80072e62e1f1df4de3d18c7ede8189f5c864b
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-01-07 18:46:41 -0300

    app.js arreglado

--- BuyTech/app.js
+++ BuyTech/app.js
@@ -1,10 +1,10 @@
-const express = requir('express');
+const express = require('express');
 const app = express();
 const path = require('path');
-
-app.listen(3000);
-
 const publicPath = path.resolve(__dirname, 'public');
-app.user(express.static(publicPath));
+
+app.listen(3000,()=> console.log("Server running"));
+
+app.use(express.static(publicPath));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
