cc103d6d5cd3191a2ca6b6c6985bbcf9b5a10eae
Saporiti <pausapo@yahoo.com.ar>
2022-02-19 12:56:19 -0300

    Se corrije error en mainRouter.js

--- myApp/routes/mainRouter.js
+++ myApp/routes/mainRouter.js
@@ -1,5 +1,4 @@
 const express= require ('express');
-const mainController = require('../controllers/mainController.js');
 const router = express.Router();
 
 const mainController = require('../controllers/mainController.js')
