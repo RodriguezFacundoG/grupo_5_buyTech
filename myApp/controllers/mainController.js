b070ffaf8b1199fc4e7fbb8443badf2de550e481
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-02-22 14:57:53 -0300

    dinamisando links

--- myApp/controllers/mainController.js
+++ myApp/controllers/mainController.js
@@ -1,16 +1,9 @@
-const express = require('express');
-const path = require ('path');
-const fs = require ('fs');
- 
-
-//datos
 const productData = require ('../data/productsData');
 
 
 const mainController= {
-
 home: (req,res)=>{
-    res.render('index')
+    res.render('index', {productData})
     },
 };
 
--- myApp/data/productsData.js
+++ myApp/data/productsData.js
@@ -1,6 +1,45 @@
-const products ={
 
+const productData =[{
+    id:1,
+    name:'Samsung Galaxy s10',
+    description:'El mejor de los mejores',
+    img:'img-samsung-galaxy-s10.jpg',
+    top: true,
+    discount: true,
+    discountPercent: 50,
+    latest: true,
+    },
+    {
+    id:2,
+    name:'Samsung Galaxy Tab 10.1',
+    description:'LA Tableta',
+    img:'latin_SM-T531NYKATPA_000241405_Front_black.webp',
+    top: true,
+    discount: false,
+    discountPercent: 0,
+    latest: false,
+    },
+    {
+    id:3,
+    name:'Samsung Notebook 7 13.3',
+    description:'the Lap pal ',
+    img:'samsung laptop.webp',
+    top: false,
+    discount: true,
+    discountPercent: 35,
+    latest: false,
+    },
+    {
+    id:4,
+    name:'Samsung GEAR-VR 1024X682',
+    description:'El mejor de los mejores',
+    img:'Gear-VR-1024x682.jpg',
+    top: false,
+    discount: true,
+    discountPercent: 10,
+    latest: true,
+    },
 
-}
+]
 
-module.exports = products
+    module.exports = productData
