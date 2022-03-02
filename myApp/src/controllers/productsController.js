6fbc9c12a7e6ceb9fc5dc73cbbe7e4bcd17cb42e
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-02 20:19:47 -0300

    Se actualizan metodos para luego darles funcionalidad

--- myApp/src/controllers/productsController.js
+++ myApp/src/controllers/productsController.js
@@ -2,29 +2,33 @@ const express = require('express');
 const path = require ('path');
 const fs = require ('fs');
  
-
 //datos
-const productData = require ('../data/products/productsData.json');
+const productData = require ('../data/productsData.json');
 
 
-const productsController= {
+const productsController = {
 
-typeList: (req,res)=>{
-    res.render('productTypeList')
+    index: (req,res) => {
+        res.render('productTypeList')
     },
-details: (req,res)=>{
-    res.render('productDetails')
-},
-create: (req,res)=>{
-    res.render('productCreate')
-},
-edit: (req,res)=>{
-    res.render('productEdit')
-},
-cart: (req,res)=>{
-    res.render('productCart')
-},
-
+    create: (req,res) => {
+        res.render('productDetails')
+    },
+    store: (req,res) => {
+        res.render('productCreate')
+    },
+    edit: (req,res) => {
+        res.render('productEdit')
+    },
+    detail: (req,res) => {
+        res.render('productCart')
+    },
+    update: (req,res) => {
+        res.render('productCart')
+    },
+    delete: (req,res) => {
+        res.render('productCart')
+    }
 };
 
 
