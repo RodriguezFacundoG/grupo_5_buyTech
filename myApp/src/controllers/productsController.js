3b6889a911327831657cc4a0d629d4607895014d
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-03 21:05:01 -0300

    Se agrega funcionalidad parcial de métodos del controlador

--- myApp/src/controllers/productsController.js
+++ myApp/src/controllers/productsController.js
@@ -8,27 +8,79 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productsController = {
 
+    //Muestra todos los productos
     index: (req,res) => {
-        res.render('productTypeList')
+        res.render('index', {products: products})
     },
-    detail: (req,res) => {
-        res.render('productDetails')
-    },
-    create: (req,res) => {
-        res.render('productDetails')
-    },
-    store: (req,res) => {
 
+    //Muestra el detalle de un producto
+    detail: (req,res) => {
+        let idABuscar = req.params.id;
+
+        products.forEach(element => {
+			if(idABuscar == element.id){	
+                res.render('productDetails', {element: element})			
+			} 							
+		});        
+    },
+
+    //Muestra form de creacion
+    create: (req,res) => {
         res.render('productCreate')
     },
+
+    //Guarda la informacion por POST
+    store: (req,res) => {
+        let newProduct = req.body;
+        newProduct.id = products[products.length - 1].id + 1; //Le sumo 1 al id del ultimo producto del array en nuestra DB
+		
+		products.push(userToCreate)		
+
+		let productsJSON = JSON.stringify(products)
+		fs.writeFileSync(productsFilePath, productsJSON, 'utf-8') 
+    },
+
+    //Muestra form de edición para el producto seleccionado por id
     edit: (req,res) => {
-        res.render('productEdit')
+        let idABuscar = req.params.id;		
+		// if (!Number(idABuscar)) {	
+		// 	return
+		// }
+
+		products.forEach(element => {
+			if(idABuscar == element.id){				
+				res.render('productEdit', {element: element})
+			} 							
+		})
+        
     },
+
+    //Actualiza la informacion del producto a traves de PUT
     update: (req,res) => {
-        res.render('productCart')
+        let idABuscar = Number(req.params.id);
+		let productToEdit = req.body;	
+
+		products.forEach(element => {
+			if(element.id == idABuscar) {
+				element = productToEdit;				
+			}
+		})
+		let productsJSON = JSON.stringify(products)
+		fs.writeFileSync(productsFilePath, productsJSON, 'utf-8')
+		res.redirect('/products/' + req.params.id)
+        
     },
+
+    //Elimina el producto seleccionado por id
     delete: (req,res) => {
-        res.render('productCart')
+        let idABuscar = Number(req.params.id);
+		let newProducts = products.filter(element => {
+			return element.id != idABuscar
+		})
+		
+		let newProductsJSON = JSON.stringify(newProducts)
+		fs.writeFileSync(productsFilePath, newProductsJSON, 'utf-8')
+		res.redirect('/products/')
     }
 };
 
