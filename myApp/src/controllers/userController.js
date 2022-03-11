4e4942470aa04a570d4e737cf5cd2e9b73271595
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-03-11 01:03:02 -0300

    ROUTES GET done

--- myApp/src/controllers/userController.js
+++ myApp/src/controllers/userController.js
@@ -8,12 +8,15 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
 const userController= {
-
     login: (req,res)=>{
         res.render('login')
         },
     register: (req,res)=>{
         res.render('register')
+    },
+    productCart: (req,res)=>{
+        //hacer un IF para que si el usuario está registrado o no, vaya a un lado especifico
+        res.render('productCart')
     }
 };
 
--- myApp/src/routes/userRouter.js
+++ myApp/src/routes/userRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const userController = require('../controllers/userController.js')
 
+router.get('/login', userController.login);
 router.post('/login', userController.login);
+router.get('/register', userController.register);
 router.post('/register', userController.register);
+router.get('/productcart', userController.productCart);
 
 module.exports= router;
