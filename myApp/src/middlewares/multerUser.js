704f216cc8967d69e576aee5570cd427640655ec
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-03-20 20:22:16 -0300

    MULTER to module Middleware

--- myApp/src/middlewares/multerUser.js
+++ myApp/src/middlewares/multerUser.js
@@ -1,16 +1,20 @@
 const multer = require ('multer');
+const path = require ('path')
 
 const storage= multer.diskStorage({
     destination: (req,file,cb)=>{
-        cb(null, path.resolve('../data/usersAvatars'))
+          //cb(null, path.join('public/images/userAvatars'))  
+          cb(null, path.join('src/data/usersAvatars'))  
+        //cb(null, path.resolve('public/images/userAvatars'))
+        //NOTA: el resolve funciona, pero es muy largo el path que escribe
     },
-
-    fileName: (req,file,cb)=>{
-        cb(null, file.fieldname + '---'+ Date.now())
+    fileName: (req,file,cb)=>
+    {cb(null,file.originalname.replace(/ /g, "aaa") + Date.now() + path.extname(file.originalname));
     }
 }
 )
+const uploadUser = multer({storage:storage});
 
-const upload = multer({storage:storage});
+module.exports = uploadUser
 
-module.exports = upload
+
--- myApp/src/routes/userRouter.js
+++ myApp/src/routes/userRouter.js
@@ -1,33 +1,14 @@
 const express= require ('express');
 const router = express.Router();
 const path = require ('path');
-
-// Multer
-const multer = require ('multer');
-
-const storage= multer.diskStorage({
-    destination: (req,file,cb)=>{
-          //cb(null, path.join('public/images/userAvatars'))  
-          cb(null, path.join('src/data/usersAvatars'))  
-        //cb(null, path.resolve('public/images/userAvatars'))
-        //NOTA: el resolve funciona, pero es muy largo el path que escribe
-    },
-    fileName: (req,file,cb)=>
-    {cb(null,file.originalname.replace(/ /g, "aaa") + Date.now() + path.extname(file.originalname));
-    }
-}
-)
-const upload = multer({storage:storage});
-// ! Multer
-
-//const uploadUser = require('../middlewares/multerUser')
+const multerUploadUser = require('../middlewares/multerUser')
 const userController = require('../controllers/userController.js')
 
 router.get('/login', userController.login);
 router.post('/login', userController.login);
 
 router.get('/register', userController.register);
-router.post('/register', upload.single('user_photo'), userController.register1);
+router.post('/register', multerUploadUser.single('user_photo'), userController.register1);
 
 router.get('/product', userController.productCart);
 
