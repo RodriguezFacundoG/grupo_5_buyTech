ef3069fbe6d9b37aba2c8caacd8109e0def5ae0d
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-03-28 19:01:30 -0300

    VERIFICATIONS...

--- myApp/src/middlewares/verificationRegister.js
+++ myApp/src/middlewares/verificationRegister.js
@@ -3,7 +3,8 @@ const {body} = require('express-validator');
 const validateRegister = [
     body('user_full_name').notEmpty().withMessage('Debes ingresar tu nombre completo'),
     body('user_mail').isEmail().withMessage('Debes ingresar un email válido'),
-    body('user_password').notEmpty().withMessage('Debes ingresar tu contraseña').bail().isLength({min: 6}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    body('user_password').notEmpty().withMessage('Debes ingresar tu contraseña').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
+    body('user_password_verification').notEmpty().withMessage('Debes ingresar tu verificación').bail().isLength({min: 6}).withMessage('La contraseña debe tener al menos 4 caracteres'),
 ]
 
 module.exports = validateRegister;
