605f65a28bf5c3d4d226b66019f071cdf67f5944
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-28 20:00:00 -0300

    Se arregla multer para corregir nombre en archivos

--- myApp/src/middlewares/multerUser.js
+++ myApp/src/middlewares/multerUser.js
@@ -2,21 +2,21 @@ const multer = require ('multer');
 const path = require ('path')
 
 const storage= multer.diskStorage({
-    destination: (req,file,cb)=>{
-          //cb(null, path.join('public/images/userAvatars'))  
-          cb(null, path.join('./../public/images/userAvatars'))  
+    destination: function(req, file, cb) {
+          
+        cb(null, path.join('public/images/userAvatars'))  
         //cb(null, path.resolve('public/images/userAvatars'))
         //NOTA: el resolve funciona, pero es muy largo el path que escribe
     },
-    fileName: (req,file,cb)=>
-    {   
+    filename: function (req, file, cb) {   
         // cb(null,file.originalname.replace(/ /g, "aaa") + Date.now() + path.extname(file.originalname));
         let imageName = `userImg-${Date.now()}${path.extname(file.originalname)}`;
+        
         cb(null, imageName);
     }
 });
 const uploadUser = multer({storage:storage});
 
-module.exports = uploadUser
+module.exports = uploadUser;
 
 
