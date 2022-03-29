a181e6183ae272baea0063a365fbe20497d8eb16
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-28 23:09:02 -0300

    Agrego login

--- myApp/src/controllers/userController.js
+++ myApp/src/controllers/userController.js
@@ -1,40 +1,42 @@
-const path = require ('path');
-const fs = require ('fs');
-const {validationResult} = require('express-validator');
- 
+const path = require('path');
+const fs = require('fs');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
 //datos
 const productsFilePath = path.join(__dirname, '../data/productsData.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
 
-const userController= {
-    login: (req,res)=>{
-        res.render('login')
+const userController = {
+    
+    login: (req, res) => {  
+        res.render('login');             
     },
-    registerForm: (req,res)=>{
+    registerForm: (req, res) => {
         return res.render('register');
     },
-    registerUpload: (req,res)=>{
+    registerUpload: (req, res) => {
         /* let newUser = req.body;
         newUser.id = users[users.length - 1].id + 1;
         users.push(newUser);
         let usersJSON = JSON.stringify(users);
-        fs.writeFileSync(usersFilePath, usersJSON, 'utf-8'); */        
+        fs.writeFileSync(usersFilePath, usersJSON, 'utf-8'); */
         console.log(req.body);
         console.log(req.file);
-        
+
         let errors = validationResult(req);
-        
-        if(!errors.isEmpty()){
+
+        if (!errors.isEmpty()) {
             console.log(errors.mapped());
-           return res.render('register', {errors: errors.mapped(), old: req.body});
-        }else{
+            return res.render('register', { errors: errors.mapped(), old: req.body });
+        } else {
             return res.send('Registro exitoso');
         }
-       
+
     },
-    productCart: (req,res)=>{
+    productCart: (req, res) => {
         //hacer un IF para que si el usuario está registrado o no, vaya a un lado especifico
         //hacer IF con sesion de usuario o redirigir a Register
         //hacer res.redirect a Login
@@ -42,4 +44,4 @@ const userController= {
     }
 };
 
-module.exports= userController;
+module.exports = userController;
--- myApp/src/middlewares/loginAuth.js
+++ myApp/src/middlewares/loginAuth.js
@@ -5,8 +5,18 @@ function loginAuth (req, res, next) {
     const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersData.json"));
     const users = JSON.parse(usersJSON);
     for (let user of users) {
-        if(req.body.)
+        if (req.body.mail == user.email) {
+            if (bcrypt.compare(req.body.password, user.password)) {
+                res.render('login');
+            } else {
+                res.send("Credenciales inválidas")                 
+            }
+
+        } else {
+            res.render('error');
+        }
     }
+    
     next();
 }
 
