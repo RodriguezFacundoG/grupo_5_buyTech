67b7ddd04d2cc9ad4d8f257179c92f9fa98fc671
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-30 22:37:14 -0300

    Se agrega middleware de Auth para login

--- myApp/src/routes/userRouter.js
+++ myApp/src/routes/userRouter.js
@@ -1,19 +1,20 @@
-const express= require ('express');
+const express = require('express');
 const router = express.Router();
 const multerUploadUser = require('../middlewares/multerUser');
 const userController = require('../controllers/userController.js');
 const verificationRegister = require('../middlewares/verificationRegister');
+const loginAuth = require('../middlewares/loginAuth')
 
 router.get('/login', userController.login);
-router.post('/login', userController.login);
+router.post('/login', loginAuth, userController.login);
 
 router.get('/register', userController.registerForm);
 //debido a que Multer agrega campos al req.body:
 // el orden de Multer y Verification debe ser asi: 1ero Multer, 2do Verification.
-router.post('/register',multerUploadUser.single('user_photo'), verificationRegister,  userController.registerUpload);
+router.post('/register', multerUploadUser.single('user_photo'), verificationRegister, userController.registerUpload);
 
 router.get('/product', userController.productCart);
 
-module.exports= router;
+module.exports = router;
 
 
