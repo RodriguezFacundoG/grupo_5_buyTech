a664df9110e28e144c8d60bf7f0bfb1474a17657
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-03-31 13:10:07 -0300

    Agregado el session

--- myApp/src/middlewares/loginAuth.js
+++ myApp/src/middlewares/loginAuth.js
@@ -1,21 +1,46 @@
+const bcrypt = require('bcrypt');
 const fs = require('fs')
 const path = require('path');
 
 function loginAuth (req, res, next) {
-    const usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersData.json"));
-    const users = JSON.parse(usersJSON);
-    for (let user of users) {
-        if (req.body.mail == user.email) {
-            if (bcrypt.compare(req.body.password, user.password)) {
-                res.render('login');
-            } else {
-                res.send("Credenciales inválidas")                 
-            }
-
-        } else {
-            res.render('error');
-        }
+    let usersJSON = fs.readFileSync(path.join(__dirname, "../data/usersData.json"));
+    let users;
+    //Si está vacío, que en la variable users, ponga un array vacío, para despues 
+    //poder agregarle varios objetos (cada uno un usuario)
+    if(usersJSON == ''){
+        users = [];
+    } else {
+        users = JSON.parse(usersJSON);
     }
+    
+    for (let user of users) {
+        if (req.body.email == user.email) {
+            let samePassword = bcrypt.compareSync(req.body.password, user.password)
+            
+            if (samePassword) {    
+                            
+                req.session.userLogged = user;
+                // res.render("user")
+                break;                
+            } 
+
+            return res.render("login", {
+                errors: {
+                    email: {
+                        msg: "Credenciales inválidas"
+                    }
+                }
+            });
+        }        
+    }
+    
+    return res.render('login', {
+        errors: {
+            email: {
+                msg: "Este email no está registrado"
+            }
+        }
+    });
     
     next();
 }
