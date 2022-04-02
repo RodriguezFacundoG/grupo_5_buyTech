c1b82416ed79cb589f5e32783527f020b3c230a4
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-04-01 22:27:16 -0300

    Se agrega comentario con bloque de codigo optativo en caso de ser necesario para implementacion final

--- myApp/src/middlewares/userLoggedMiddleware.js
+++ myApp/src/middlewares/userLoggedMiddleware.js
@@ -1,7 +1,16 @@
+const session = require("express-session");
+
 function userLoggedMiddleware (req, res, next) {
     res.locals.isLogged = false;    //res.locals son variables que puedo compartir entre las vistas, indistintamente del controlador
-    if(req.session.userLogged){
+    if(req.session.userLogged){     //if(req.session && req.session.userLogged)
         res.locals.isLogged = true;
+        /*
+        A continuación, otra forma de en la vistas, con el uso de locals.userLogged, user los datos del usuario en session
+        De esta manera, no es necesario pasar la info como objeto en el metodo render().
+        res.locals is an object that contains response local variables scoped to the request and because of this,
+        it is only available to the view(s) rendered during that request/response cycle
+            res.locals.userLogged = req.session.userLogged;
+        */
     } 
     next();
 }
