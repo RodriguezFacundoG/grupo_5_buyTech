ff73da44e72af67a2c0ebc7eaadaa5f02a49775e
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-04-02 15:35:08 -0300

    Se vuelve a una version anterior, porque con el uso del MW de aplicacion, no hace falta pasarle el objeto session en el método render

--- myApp/src/controllers/mainController.js
+++ myApp/src/controllers/mainController.js
@@ -1,19 +1,19 @@
-const path = require ('path');
-const fs = require ('fs');
+const path = require('path');
+const fs = require('fs');
 
 //datos
 const productsFilePath = path.join(__dirname, '../data/productsData.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const mainController= {
-home: (req,res)=>{
-    res.render('index', {products: products})
+const mainController = {
+    home: (req, res) => {
+        res.render('index', { products: products });
     },
-types: (req,res)=>{
-    let categoryId = req.params.id;
-    res.render('productTypeList', {products, categoryId})
+    types: (req, res) => {
+        let categoryId = req.params.id;
+        res.render('productTypeList', { products, categoryId })
     },
 };
 
 
-module.exports= mainController;
+module.exports = mainController;
