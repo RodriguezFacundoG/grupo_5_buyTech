888cf6e0936c8d291b4b7104786123fc5e11c966
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-04-27 16:40:44 -0300

    Se modifican claves de los att name a validar

--- myApp/src/middlewares/verificationRegister.js
+++ myApp/src/middlewares/verificationRegister.js
@@ -1,10 +1,11 @@
 const {body} = require('express-validator');
 
 const validateRegister = [
-    body('user_full_name').notEmpty().withMessage('Debes ingresar tu nombre completo'),
-    body('user_mail').isEmail().withMessage('Debes ingresar un email válido'),
-    body('user_password').notEmpty().withMessage('Debes ingresar una contraseña').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
-    body('user_password_verification').notEmpty().withMessage('Debes ingresar tu verificación').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
+    body('first_name').notEmpty().withMessage('Debes ingresar tu nombre'),
+    body('last_name').notEmpty().withMessage('Debes ingresar tu apellido'),
+    body('email').isEmail().withMessage('Debes ingresar un email válido'),
+    body('password').notEmpty().withMessage('Debes ingresar una contraseña').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
+    body('password_verification').notEmpty().withMessage('Debes ingresar tu contraseña nuevamente').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
 ]
 
 module.exports = validateRegister;
