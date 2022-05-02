2e9e53bd00cc91dfeb38e3f533c00756d3d05a0f
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-05-02 00:12:39 -0300

    Se cambió cada uno de los métodos para funcionar con las db

--- myApp/src/controllers/productsController.js
+++ myApp/src/controllers/productsController.js
@@ -1,26 +1,23 @@
 const path = require("path");
-const fs = require("fs");
-
-//datos
-const productsFilePath = path.join(__dirname, "../data/productsData.json");
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 const db = require("../database/models/index")
 
 const productsController = {
   //Muestra todos los productos
   index: (req, res) => {
-    res.render("index", { products: products });
+    db.Product.findAll()
+      .then( (products) => {
+        return res.render("index", { products });
+      })
   },
 
   //Muestra el detalle de un producto
   detail: (req, res) => {
     let idABuscar = req.params.id;
 
-    products.forEach((element) => {
-      if (idABuscar == element.id) {
-        res.render("productDetails", { element: element });
-      }
-    });
+    db.findByPk(idABuscar)
+      .then( (producto) => {
+        return res.render("productDetails", { element: producto });
+      });     
   },
 
   //Muestra form de creacion
@@ -53,42 +50,38 @@ const productsController = {
   //Muestra form de edición para el producto seleccionado por id
   edit: (req, res) => {
     let idABuscar = req.params.id;
-    // if (!Number(idABuscar)) {
-    // 	return
-    // }
 
-    products.forEach((element) => {
-      if (idABuscar == element.id) {
-        res.render("productEdit", { element: element });
-      }
-    });
+    db.findByPk(idABuscar)
+      .then( (producto) => {
+        return res.render("productEdit", { element: producto });
+      });    
   },
 
   //Actualiza la informacion del producto a traves de PUT
   update: (req, res) => {
-    let idABuscar = Number(req.params.id);
-    let productToEdit = req.body;
 
-    products.forEach((element) => {
-      if (element.id == idABuscar) {
-        element = productToEdit;
-      }
-    });
-    let productsJSON = JSON.stringify(products);
-    fs.writeFileSync(productsFilePath, productsJSON, "utf-8");
-    res.redirect("/products");
+    db.Product.update({
+
+      type: body.product_category,
+      name: body.product_name,
+      description: body.product_description,
+      weight: body.product_weight,
+      color: body.product_color,
+      size: body.product_size,
+      price: body.product_price,
+      discount: body.product_discount,
+      picture: filename,
+    }, {
+
+      where: { id: req.params.id }
+
+    }).then( () => res.redirect("/products") );   
   },
 
   //Elimina el producto seleccionado por id
   delete: (req, res) => {
-    let idABuscar = Number(req.params.id);
-    let newProducts = products.filter((element) => {
-      return element.id != idABuscar;
-    });
-
-    let newProductsJSON = JSON.stringify(newProducts);
-    fs.writeFileSync(productsFilePath, newProductsJSON, "utf-8");
-    res.redirect("/products");
+    db.Product.destroy({where: { id: req.params.id }})
+      .then( ()=> res.redirect("/products"))
   },
 };
 
