5eee27a5c06e3a035bbef300b59012db710ea1a8
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-05-15 17:04:55 -0300

    Se saca el metodo types

--- myApp/src/controllers/mainController.js
+++ myApp/src/controllers/mainController.js
@@ -8,11 +8,7 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const mainController = {
     home: (req, res) => {
         res.render('index', { products: products });
-    },
-    types: (req, res) => {
-        let categoryId = req.params.id;
-        res.render('productTypeList', { products, categoryId })
-    },
+    }    
 };
 
 
