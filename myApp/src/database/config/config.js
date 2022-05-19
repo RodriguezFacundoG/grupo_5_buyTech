626b17fcd3d8587613b10097892cf5ddeb189669
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-05-19 00:49:55 -0300

    Se cambia contraseña de ingreso a la DB

--- myApp/src/database/config/config.js
+++ myApp/src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "structure",
+    "password": null,
+    "database": "buytech_database",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
