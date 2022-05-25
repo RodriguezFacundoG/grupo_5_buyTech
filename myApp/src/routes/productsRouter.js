74e9a05291d9c7c1ee5e693ef394841d5ea6e196
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-05-25 14:08:47 -0300

    productCreateAndEdit Verification Backend DONE TO CHECK

--- myApp/src/routes/productsRouter.js
+++ myApp/src/routes/productsRouter.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const productsController = require('../controllers/productsController')
 const multerUploadProduct = require('../middlewares/multerProducts');
+const productCreateAndEditValidation = require('../middlewares/productCreateAndEditValidation');
 
 router.get('/categories/:type', productsController.types)
 
-router.get('/create', productsController.create);
+router.get('/create',productCreateAndEditValidation, productsController.create);
 router.post('/', multerUploadProduct.single("product_image"), productsController.store);
 
 router.get('/:id', productsController.detail);
 
-router.get('/:id/edit', productsController.edit)
+router.get('/:id/edit',productCreateAndEditValidation, productsController.edit)
 router.put('/:id', productsController.update)
 
 router.delete('/:id', productsController.delete)
