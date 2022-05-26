9ed9b7948ad6d39b29733e2027621ba80693a769
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-05-26 20:47:27 -0300

    Se agrega estructura de datos y hace la query para responder a los endpoint de usuarios

--- myApp/src/controllers/api/userApiController.js
+++ myApp/src/controllers/api/userApiController.js
@@ -6,208 +6,34 @@ const bcrypt = require('bcrypt');
 const db = require("../../database/models/index")
 // Que es lo mismo que hacer require("../database/models") porque Javascript va a buscar siempre el archivo index
 
+
 const userApiController = {
-    //Muestro form de login
-    loginForm: (req, res) => {
-        return res.render('login');
-    },
-    //Logueo al usuario y lo pongo en sesión, también creo la cookie en caso de requerirse
-    loginProcess: (req, res) => {
-
-        let errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.render('login', {errors: errors.mapped(), old: req.body})
-        }
-
-        db.User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-            .then((userToLogin) => {
-                let samePassword = bcrypt.compareSync(req.body.password, userToLogin.password)
-                
-                if (samePassword) { 
-                    delete userToLogin.password;            //Borro el password antes de guardar el usuario en sesion para mayor seguridad
-                    req.session.userLogged = userToLogin;   //creo la propiedad userLogged en el objeto global req.session
-                    //Cookie
-                    if (req.body.remember_me) {
-                        res.cookie('recordarEmail', req.session.userLogged.email, { maxAge: 60000 });
-                    }                    
-                    return res.redirect('/');
-                }
-
-                return res.render("login", {
-                    errors: {
-                        email: {
-                            msg: "Credenciales inválidas"
-                        }
-                    }
-                });
-            })
-            
-            .catch( () => { // Si quiero puedo captar el error como parámetro en el callback del catch        
-                return res.render('login', { 
-                    errors: {
-                        email: {
-                            msg: "Este email no está registrado"
-                        }
-                    }
-                })
-            });          
-
-    },
-
-    // Muestra formulario de creacion de usuario
-    registerForm: (req, res) => {
-        return res.render("register");
-    },
-    //Guardo la info de usuario registrado
-    registerUpload: async (req, res) => {
-        let errors = validationResult(req);        
-        let userPass = req.body.password;
-        let userPassVerification = req.body.password_verification;
-
-        const user = await db.User.findOne({       
-            attributes: ['id', 'first_name', 'email'], //No agrego todos los att, para no sobrecargar la consulta con info
-            where: {
-                email: req.body.email
-            }
-        })
-            
-
-        if ( !errors.isEmpty() ) {            
-            return res.render('register', { errors: errors.mapped(), old: req.body });           
-        }
-
-        if ( user != null ) {   //Si encuentra un usuario, interrumpe la lógica, si no lo encuentra (devuvelve null), sigue con las demas validaciones
-            return res.render('register', { 
-                errors: {
-                    email: {
-                        msg: "Este email no está registrado"
-                    }
-                }
-            })
-        }
-
-        if (userPass !== userPassVerification) {                          
-            return res.render('register', { old: req.body, 
-                errors: {
-                    password_verification: {
-                        msg: 'Las contraseñas no coinciden'
-                    }
-                }
-            })
-        }
-
-        let passEncripted = bcrypt.hashSync(userPass, 10);        
-        let newUser = {
-            ...req.body,
-            password: passEncripted,                
-            avatar: req.file != undefined ? req.file.filename : 'user-solid.svg', //Solo agrega esta propíedad en caso de que se cree agregue una imagen, sino
-            user_category_id: 0,                                                  // pone una por default
-        };
-        delete newUser.password_verification;   //Necesito borrar el password que se verifica, porque no está hasheado (el otro se pisa directamente)
-
-        try{   
-            await db.User.create(newUser);        
-            return res.redirect("/");        
-        }
-
-        catch(error){
-            console.log(error)
-        }  
-    },
-    //Muestro el producto con todos sus items
-    getCart: (req, res) => {           
-        db.Item.findAll({            
-            include: [
-                {
-                    model: db.User,
-                    as: 'users',                    
-                    where:{
-                        id: req.session.userLogged.id
-                    }
-                },
-                {
-                    model: db.Product,
-                    as: 'product',                    
-                }
-            ],
-        })
-            //En "items" tengo todos los items relacionados al usuario logueado; con respecto a products ya viene 
-            // por la relación, ligado a un solo producto (Ver en ERD) 
-            .then( (items) => {                                         
-                return res.render('productCart',{elements: items})               
-            })        
-    },
-    //Añado un producto al carrito cuando toco el botón en la vista de productDetails
-    addToCart: async (req, res) => {
-        let productIdToCreate = req.params.id;
-        const item = await db.Item.create({
-            product_id: productIdToCreate,    
-
-        },
-        {
-            include: [
-                {
-                    association: 'users',
-                    where: {
-                        id: req.session.userLogged.id
-                    }
-                }
-            ]
-        })
-        await item.save()
-        console.log(item);
-        item.setUsers([req.session.userLogged.id])
-        res.redirect('/');
-    },
-    //Destruyo la sesión y la cookie
-    logout: (req, res) => {
-        res.clearCookie("recordarEmail");
-        req.session.destroy();
-        res.redirect('/');
-    },
-
-    /* Muestra el Perfil del Usuario */
-    profile: (req, res) => {
-        let idABuscar = req.params.id;
-        db.User.findByPk(idABuscar)
-          .then( (user) => {        
-              return res.render("userProfile", { user: user });
-          })     
-    },
-
-    /* Muestra el Formulario de Edición para el Usuario */
-    edit: (req, res) => {
-        let idABuscar = req.params.id;
-        db.User.findByPk(idABuscar)
-          .then( (user) => {        
-              return res.render("userEdit", { user: user });
-          })     
-    },
-
-    /* Actualiza los datos del Usuario */
-    update: (req, res) => {
-        db.User.update ({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email:req.body.email,
-            password: req.body.password
-        }, {
-            where:{
-                id: req.params.userId
-            }
-        })
-        res.redirect('/users/profile'+ req.params.userId)
-    },
-
-    /*ACA ES EL METODO QUE PUSO JUANPA PARA PROBAR LA CONEXION A LA DB*/ 
-
     listUsers: async (req, res) => {
         const users = await db.User.findAll();
-        return res.json(users)
+        const newUsersArray = users.map( (user) => {
+            return  {
+                id: user.id,
+                name: user.first_name + " " + user.last_name,
+                email: user.email,
+                detail: "/api/users/" + user.id
+            }
+        });
+        return res.json({
+            count: users.length,
+            users: newUsersArray
+        });
+
+    },
+    showOneUser: async (req, res) => {
+        let userIdToFind = req.params.id;
+        const user = await db.User.findByPk(userIdToFind)        
+        return res.json({
+            id: user.id,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            email: user.email,
+            img_url: user.avatar,
+        })
     }
 };
 
