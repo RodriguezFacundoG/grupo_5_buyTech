ba31bfdc27eaef7651f782b9edaa087d1db2d8ff
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-06-12 20:15:44 -0300

    Se cambia validacion para contraseña, ahora es minimo de 8 caracteres, lo mismo que la validacion del front

--- myApp/src/middlewares/verificationRegister.js
+++ myApp/src/middlewares/verificationRegister.js
@@ -4,8 +4,8 @@ const validateRegister = [
     body('first_name').notEmpty().withMessage('Debes ingresar tu nombre'),
     body('last_name').notEmpty().withMessage('Debes ingresar tu apellido'),
     body('email').isEmail().withMessage('Debes ingresar un email válido'),
-    body('password').notEmpty().withMessage('Debes ingresar una contraseña').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
-    body('password_verification').notEmpty().withMessage('Debes ingresar tu contraseña nuevamente').bail().isLength({min: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
+    body('password').notEmpty().withMessage('Debes ingresar una contraseña').bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    body('password_verification').notEmpty().withMessage('Debes ingresar tu contraseña nuevamente').bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
 ]
 
 module.exports = validateRegister;
