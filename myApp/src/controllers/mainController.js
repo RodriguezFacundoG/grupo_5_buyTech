df1497d3d802b5ec000b395756b2514c6c8bad0e
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-06-13 02:04:16 -0300

    Se modifica metodo en mainController p/randomizar muestras de productos

--- myApp/src/controllers/mainController.js
+++ myApp/src/controllers/mainController.js
@@ -1,19 +1,23 @@
-const path = require('path');
-const db = require("../database/models/index")
+const path = require("path");
+const db = require("../database/models/index");
 
 const mainController = {
-    home: (req, res) => {
-        db.Product.findAll({
-            order: [
-                ["discount","DESC"]
-            ],
-            limit:4
-        })
-        .then ( products => {
-            res.render('index', {products:products})
-          })
+  home: (req, res) => {
+    let rnd1 = Math.round(Math.random() * 10);
+    while (rnd1 == 0 || rnd1 > 4) {
+      rnd1 = Math.round((Math.random() * 10));
     }
+    db.Product.findAll({
+      order: [["discount", "DESC"]],
+      limit: 4,
+      where: {
+        product_category_id: rnd1,
+      },
+    }).then((products) => {
+      //res.send(products);
+      res.render("index", { products: products });
+    });
+  },
 };
-
 
 module.exports = mainController;
