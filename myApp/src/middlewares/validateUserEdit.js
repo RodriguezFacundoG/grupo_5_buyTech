4b5c87a7cd49a21b9d06fb2536f195353aab7774
RodriguezFacundoG <facundo.99.asd@gmail.com>
2022-06-13 00:27:18 -0300

    Se vuelve a pedir contraseñas como campo obligatorio

--- myApp/src/middlewares/validateUserEdit.js
+++ myApp/src/middlewares/validateUserEdit.js
@@ -3,8 +3,8 @@ const {body} = require('express-validator');
 const validateUserEdit = [
     body('first_name').notEmpty().withMessage('Debes ingresar tu nombre'),
     body('last_name').notEmpty().withMessage('Debes ingresar tu apellido'),    
-    body('password').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
-    body('password_verification').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    body('password').notEmpty().withMessage('Debes ingresar una contraseña').bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
+    body('password_verification').notEmpty().withMessage('Debes ingresar tu contraseña nuevamente').bail().isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
 ]
 
 module.exports = validateUserEdit;
